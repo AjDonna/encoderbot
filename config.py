80166d895498ae8788fa3fe65b0f6fec0fb03fc2
AjDonna <89831798+AjDonna@users.noreply.github.com>
2024-07-01 19:09:45 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ class Config(object):
     API_ID    = os.environ.get("API_ID", "7603458")  # ⚠️ Required
     API_HASH  = os.environ.get("API_HASH", "910e420f1f74f40305a684a331dade35") # ⚠️ Required
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "7331076314:AAEBAr6XiI2hvkZZDL2ICMzKhmsoxlB0z4Y") # ⚠️ Required
-    FORCE_SUB = os.environ.get('FORCE_SUB', '-1001865657918') # ⚠️ Required
+    FORCE_SUB = os.environ.get('FORCE_SUB', '-1002233381952') # ⚠️ Required
     AUTH_CHANNEL = int(FORCE_SUB) if FORCE_SUB and id_pattern.search(
     FORCE_SUB) else None
    
